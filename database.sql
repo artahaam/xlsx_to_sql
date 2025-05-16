dd47a3260811e5653d8d5cca3497b615d21aa385
alireza tahami <alireza.thm03@gmail.com>
2025-05-16 21:06:19 +0330

    removed columns : problems.judge_score, students.username

--- database.sql
+++ database.sql
@@ -1,13 +1,11 @@
 -- Active: postgres@@127.0.0.1@5432@xlsx_to_sql
 CREATE TABLE students(
     student_id VARCHAR(9) PRIMARY KEY,
-    username VARCHAR(50),
-    fullname VARCHAR(60)
+    fullname VARCHAR(60),
 )
 
 
 CREATE TABLE problems(
-    judge_score INTEGER,
     final_score INTEGER,
     student_id VARCHAR(9) REFERENCES students(student_id)
 )
