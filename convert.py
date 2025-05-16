28ec4966c35e065ed69b16f8908523c9cc26c811
alireza tahami <alireza.thm03@gmail.com>
2025-05-16 14:19:53 +0330

    database connection and queries to add data to tables

--- convert.py
+++ convert.py
@@ -1,5 +1,34 @@
 import pandas as pd
+import psycopg2
+from dotenv import load_dotenv
+from os import getenv
+load_dotenv()
 
-df = pd.read_excel('sheets/assignment_1_results.xlsx')
-df.columns.ravel()
-print(df.columns.ravel())
+connection = psycopg2.connect(database = getenv("DATABASE"), 
+                        user = getenv("USER"), 
+                        host= getenv("HOST"),
+                        password = getenv("PASSWORD"),
+                        port = getenv("PORT"))
+
+df = pd.read_excel('sheets/assignment_1_results.xlsx', na_values=['nan', '?', ''])
+
+columns = df.columns.ravel()
+username = columns[0]
+
+curser =  connection.cursor()
+
+# target indices: 0, 1, 2, 4, 6
+rows = df.values.tolist()
+for row in rows:
+    username = row[0]
+    student_id = row[1]
+    fullname = row[2]
+    final_score = row[4]
+    judge_score = row[6]
+    
+    curser.execute(f"INSERT INTO students VALUES('{student_id}', '{username}', '{fullname}')")
+    curser.execute(f"INSERT INTO problems(judge_score, final_score ,student_id) VALUES({judge_score}, {final_score}, '{student_id}')")
+    
+connection.commit()
+curser.close()
+connection.close()
