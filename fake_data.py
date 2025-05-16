0783345b888373f5548970414aef232e536b62cc
alireza tahami <alireza.thm03@gmail.com>
2025-05-16 21:04:53 +0330

    set random score

--- fake_data.py
+++ fake_data.py
@@ -1,10 +1,9 @@
 from openpyxl import Workbook
-
-# Create and managing the xlsx file for 3 files
+import random
 for f in range(1, 4):
     wb = Workbook()
     ws = wb.active
     ws.title = f"fake_data{f}.xlsx"
     for i in range(1, 20):
-        ws.append([f"stu_id{i}", f"fn{i}", 60*(i%3)])
+        ws.append([f"stu_id{i}", f"fn{i}", random.randint(0,100)])
     wb.save(f"fake_data{f}.xlsx")
