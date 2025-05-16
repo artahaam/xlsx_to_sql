46ef0c182b0276f9d75764e8a842b0bdfab7894a
alireza tahami <alireza.thm03@gmail.com>
2025-05-16 21:03:57 +0330

    remove redundant prints

--- convert.py
+++ convert.py
@@ -15,37 +15,32 @@ connection = psycopg2.connect(database = getenv("DATABASE"),
 cursor = connection.cursor()
 
 # extracting student data and inserting to the students table in the database
-df = pd.read_excel('sheets/students.xlsx', na_values=['nan', '?', ''])
+df = pd.read_excel('fake_data0.xlsx', na_values=['nan', '?', ''])
 
 rows = df.values.tolist()
-print(rows)
-for row in rows:
-    print(row)
-    username = row[0]
-    student_id = row[1]
-    fullname = row[2]
 
-    cursor.execute(f"INSERT INTO students VALUES('{student_id}', '{username}', '{fullname}')")
+for row in rows:
+    student_id = row[0]
+    fullname = row[1]
+
+    cursor.execute(f"INSERT INTO students VALUES('{student_id}', '{fullname}')")
 
 connection.commit()
 
     
 # extracting problem assignments data and inserting to the problems table in the database
-for assignment_number in range(1, 10):
+for assignment_number in range(1, 4):
 
-    df = pd.read_excel(f'sheets/assignment_{assignment_number}_results.xlsx', na_values=['nan', '?', ''])
+    df = pd.read_excel(f'fake_data{assignment_number}.xlsx', na_values=['nan', '?', ''])
   
     rows = df.values.tolist()
   
     for row in rows:
-        username = row[0]
-        student_id = row[1]
-        fullname = row[2]
-        
+        student_id = row[0]
+
         # Taking care of nan (null) values 
-        final_score = str(row[4]) if str(row[4]) != "nan" else 0
-        judge_score = str(row[6]) if str(row[6]) != "nan" else 0
-        cursor.execute(f"INSERT INTO problems(judge_score, final_score, student_id) VALUES({judge_score}, {final_score}, '{student_id}')")
+        final_score = str(row[2]) if str(row[2]) != "nan" else 0
+        cursor.execute(f"INSERT INTO problems(final_score, student_id) VALUES({final_score}, '{student_id}')")
 
 connection.commit()
 
